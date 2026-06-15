5f140cb1986459928f2bc93de4bc3bcbc5225bff
andrewnassif09-droid <andrewnassif09@gmail.com>
2026-06-15 23:42:51 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
 
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Attendance Server Running");
+});
+
+app.post("/attendance", (req, res) => {
+  console.log(req.body);
+
+  res.json({
+    success: true,
+    message: "Attendance received"
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
